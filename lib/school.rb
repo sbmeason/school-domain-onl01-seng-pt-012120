777d9211af5b0b837a93e0d5b2349e39ed26ac5a
Samantha Meason <sbmeason@gmail.com>
2020-02-12 00:53:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :name, :roster, :grade
+  
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+  # hash = {grade => [name]}
+  # @roster = hash
+if @roster[grade]
+  @roster[grade] << name
+else
+  @roster[grade] = [name]
+  end
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |grade,name|
+    name.sort!
+  end
+end
+end
+
+  
